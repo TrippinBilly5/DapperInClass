b20420ac519cc9c9a1a25eead0732ca1e56f6451
mark aldridge <mark.aldridge1@protonmail.com>
2024-08-09 13:46:28 -0700

    add code

---
+++ DapperInClass/DepartmentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperInClass
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection; //field
+        
+        //Constructor
+        public DepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+    }
+}
